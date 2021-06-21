c6506134c6472575f8a1717f64353544f76daf45
Alejandro Perez <alejandro.perez1@udea.edu.co>
2021-06-21 17:37:25 -0500

    Quit width the default button class, and upgrade the storybook version

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,11 +1,14 @@
 
 module.exports = {
-  "stories": [
-    "../stories/**/*.stories.mdx",
-    "../stories/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials"
-  ]
+
+  stories: ['../stories/**/*.stories.@(js|mdx)', '../stories/**/*.stories.@(js|jsx|ts|tsx)'],
+  addnos: ['@storybook/addon-essentials', '@storybook/addnon-links']
+  // "stories": [
+  //   "../stories/**/*.stories.mdx",
+  //   "../stories/**/*.stories.@(js|jsx|ts|tsx)"
+  // ],
+  // "addons": [
+  //   "@storybook/addon-links",
+  //   "@storybook/addon-essentials"
+  // ] 
 }
