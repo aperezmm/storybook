33fc719fdb9588f49785e6f9ebd5ee856679becf
Alejandro Perez <alejandro.perez1@udea.edu.co>
2021-06-21 22:46:49 -0500

    Input names, worked tokens colors, names

---
+++ stories/atoms/Input.stories.js
@@ -0,0 +1,12 @@
+import Input from '../../atoms/Input'
+import React from 'react'
+export default {
+  title: 'Atoms/Input',
+  component: Input,
+}
+
+export const Text = () => (
+  <Input type="text" placeholder="Nombres">
+    Input primary
+  </Input>
+)
